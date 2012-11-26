bbabd5e0571b8096bdacc8466a0f38ffc4e60ce4
inafalcao <inafalcao@gmail.com>
2012-11-25 21:29:42 -0300

    Final

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 *	Tic Tac Toe Game
 *	
 *
-*		@author Inaciane Monteiro, Joaquim Filipe
+*		@authors Inaciane Monteiro - 344079, Joaquim Filipe - 344066
 */
 
 #include <ctype.h>
@@ -19,6 +19,7 @@
 #define LOSE -1
 #define UNFINISHED 2
 #define N 3
+#define EXIT '3'
 
 #define VALID_COORD(x,y) ((x < 0 || x > N-1 || y < 0 || y > N-1) ? FALSE : TRUE)
 #define IS_EMPTY(square) (square == ' ' ? TRUE : FALSE)
@@ -29,11 +30,16 @@ void init_board(char board[N][N]);
 void print_board(char board[N][N]);
 void print_initial_information();
 int choose_mode();
-char choose_player_character();
+int who_goes_first();
+char character_choose();
+void winner_counter(char ch);
+int play_again();
 
 void user_computer();
 int valid_square(char c, int number);
 int player_move(char board[N][N], char player);
+void tip(char board[N][N], char player);
+int preview_computer_win(char board[N][N], char user, char computer);
 int comp_move(char board[N][N], char player);
 int enter_grid_move(char board[N][N], char player, char c, int number);
 int enter_coord_move(char board[N][N], char player, int x, int y);
@@ -41,19 +47,17 @@ int check_winner(char board[N][N], char player);
 int my_max(char board[N][N], char player);
 int my_min(char board[N][N], char player);
 void minimax(char board[N][N], char player);
-
-
 void computer_computer();
-int check_winner_2(char board[N][N]);
-void random_move(char board[N][N], char ch);
-int block_line_1(char board[N][N], char ch);
-int block_line_2(char board[N][N], char ch);
-int block_line_3(char board[N][N], char ch);
-int block_player(char board[N][N], char ch);
-int draw_game(char board[N][N]);
-void move_computer(char board[N][N], char ch);
 
+/*
+* Counters
+*/
+int win_x = 0, win_o = 0;
 
+/*
+*   Main code
+*
+*/
 
 int main()
 {
@@ -64,16 +68,13 @@ int main()
 	init_board(board);
 	print_initial_information();
 	
-	print_board(board); //Retirar depois
+	print_board(board);
 	
     switch(choose_mode()) {
-        case 1: computer_computer() /*executar modo 1 = computer x computer*/ ; break;
+        case 1: computer_computer(); break;
         case 2: user_computer(); break;
         default:  ;
     }
-    
-   
-    system("pause");
 
 }
 
@@ -94,10 +95,10 @@ void print_initial_information() {
 int choose_mode() {
     int mode;
     while(TRUE) {
-        printf("Choose Mode:\n[1] Computer x Computer \n[2] User x Computer\n");
+        printf("Choose:\n[1] Computer x Computer \n[2] User x Computer \n[3] Exit\n");
         scanf("%d", &mode);
-        fflush(stdin);
-        if(mode == 1 || mode == 2) {
+        __fpurge(stdin);
+        if(mode == 1 || mode == 2 || mode == 3) {
             break;
         }
         printf("\nIncorrect choice, Please Try Again\n\n");
@@ -105,38 +106,38 @@ int choose_mode() {
     return mode;
 }
 
-char choose_player_character() {
-    char choice;
-    while(TRUE) {
-        printf("\nWould you like to play as X and go first? (y/n): ");
-        scanf("%c", &choice);
-        choice = tolower(choice);
-        if(choice == 'y' || choice == 'n') {
-        	choice = (choice == 'y' ? PLAYER_X : PLAYER_O);
-        	break;
-        }
-        printf("\nIncorrect choice, Please Try Again\n\n");
-    }
-    //79 é o e 88 é x
-    return choice;
-}
+/*
+*  Performs the user moves, asks about the use of tips and informs victory
+*
+*   @return true if the user won or if it's a draw game, false otherwise
+*/
 
 int player_move(char board[N][N], char player)
 {
-	int grid_num; //celula
+	int grid_num;
 	char character;
 	int number;
 	while(TRUE)
 	{
+        printf("\nEnter ? for tip.\nOR\n");
 		printf("Enter coordenates. ex: A,2\n");
 		scanf("%c,%d", &character, &number);
-		//printf("\t\t\tYOUR TURN\n");	
-	
-    	if(enter_grid_move(board, player, character, number) == 0)
-			break;
+		__fpurge(stdin);
+         
+	    if(character == '?'){
+            tip(board, player);
+            break;
+        }
 
-		// Invalid move location
-		printf("Incorrect choice, Try Again\n");
+        //if exit
+        if(character == EXIT)
+            exit(0);
+        
+        if(enter_grid_move(board, player, character, number) == FALSE)
+    			break;
+    
+    		// Invalid move location
+    		printf("Incorrect choice, Try Again\n");
 	}
 
 	print_board(board);
@@ -144,6 +145,7 @@ int player_move(char board[N][N], char player)
 	int play = check_winner(board, player);
 	if(play == WIN){
 		printf("\nCongratulations, you are the winner\n");
+		winner_counter(player);
 		return TRUE;
 	}else if(play == DRAW){
 		printf("\nIt's a Draw\n");
@@ -152,28 +154,105 @@ int player_move(char board[N][N], char player)
 	return FALSE;
 }
 
+/*
+* Generates and prints the tip for the user. 
+*
+*/
+void tip(char board[N][N], char player) {
+    char other_board[N][N];
+    int i, j;
+    char choice;
+    
+    for(i=0; i<N; i++){
+             for(j=0; j<N; j++){
+                      other_board[i][j] = board[i][j];
+             }
+    }
+    
+    minimax(other_board, player);
+    
+    printf("\nDo you want to make this move below? (y/n)\n\n");
+    print_board(other_board);
+    
+    while(TRUE){
+        scanf("%c", &choice);
+        __fpurge(stdin);
+        choice = tolower(choice);
+        if(choice == 'y'){
+              minimax(board, player);
+              break;
+        }
+        if(choice == 'n') break;
+        
+        if(choice == EXIT) exit(0);
+                
+        printf("Incorrect choice. Try again.\n");
+    }
+}
+
+
+/*
+* Checks if the user is blocked, and the computer will win.
+*
+* @returns true if the computer won, false otherwise.
+*/
+int preview_computer_win(char board[N][N], char user, char computer) {
+    char other_board[N][N];
+    int i, j;
+    
+    for(i=0; i<N; i++){
+             for(j=0; j<N; j++){
+                      other_board[i][j] = board[i][j];
+             }
+    }
+    minimax(other_board, computer);
+    minimax(other_board, user);
+    minimax(other_board, computer);
+    
+    int play = check_winner(other_board, computer);
+	if(play == WIN){
+        minimax(board, computer);
+        print_board(board);
+		printf("\nYou're blocked, the computer wins.\n");
+		winner_counter(computer);
+		return TRUE;
+    }
+    return FALSE;
+     
+}
+
+/*
+*  Validate the coordinates given by the user.
+*
+* @returns true if it is valid, false otherwise.
+*/
 int valid_square(char character, int number) {
     character = tolower(character);
+    /* Coordinate character given by the user. */
     int int_value_character = character;
     int int_value_a = 'a';
     int int_value_c = 'c';
-    if(  (int_value_character > int_value_c || int_value_character < int_value_a)   ||  ( number > 3 || number < 1 ) )
+    if(  (int_value_character > int_value_c || int_value_character < int_value_a)   ||  ( number > N || number < 1 ) )
        return FALSE;
     return TRUE;
         
 }
 
+/*
+*  Performs computer moves and counts whether the computer won.
+*
+* @returns true if the computer won or if it's a draw, false otherwise.
+*/
 int comp_move(char board[N][N], char player)
 {
-	//printf("\t\t\tCOMPUTER'S TURN\n");
-
 	minimax(board, player);
 	print_board(board);
-
+	
 	int play = check_winner(board, player);
 	if(play == WIN)
 	{
 		printf("\nUnlucky, the computer wins\n");
+		winner_counter(player);
 		return TRUE;
 	}else if(play == DRAW){
 		printf("\nIt's a Draw\n");
@@ -182,6 +261,11 @@ int comp_move(char board[N][N], char player)
 	return FALSE;
 }
 
+/*
+*  Translates user coordinates to integers i,j representing the board position.
+*
+* @returns true or false
+*/
 int enter_grid_move(char board[N][N], char player, char character, int number) {
 	if(valid_square(character, number) == FALSE)
 		return TRUE;
@@ -189,7 +273,7 @@ int enter_grid_move(char board[N][N], char player, char character, int number) {
     //Calculates corresponding i,j coordinates. i = line, j = row
 	//a = 97 / b = 98 / c = 99
 	int i,j;
-	int int_value_character = character;
+	int int_value_character = tolower(character);
 	if(int_value_character == 97){
 		i = 0;
 	}else if(int_value_character == 98){
@@ -202,24 +286,32 @@ int enter_grid_move(char board[N][N], char player, char character, int number) {
 	
 	if(IS_EMPTY(board[i][j]) == FALSE)
 		return TRUE;
-	board[i][j] = player;
+	
+    board[i][j] = player;
 
 	return FALSE;
 }
 
+/*
+*  Checks if computer's moves are valid.
+*/
 int enter_coord_move(char board[N][N], char player, int x, int y)
 {
 	// Check that coordinates are valid
 	if(VALID_COORD(x,y) == FALSE)
 		return TRUE;
-	//if(board[x][y] != ' ')
 	if(IS_EMPTY(board[x][y]) == FALSE)
 		return TRUE;
 	board[x][y] = player;
-
+        
 	return FALSE;
 }
 
+/*
+*  Checks whether the player (computer or user, specified by the "player" function parameter)
+*  won, and checks for draw.
+*
+*/
 int check_winner(char board[N][N], char player)
 {
 	int i, j;
@@ -269,6 +361,11 @@ int check_winner(char board[N][N], char player)
 	return UNFINISHED;
 }
 
+
+/*
+*  Used for computer's logic.
+*
+*/
 int my_max(char board[N][N], char player)
 {
 	int result = check_winner(board, player);
@@ -305,6 +402,11 @@ int my_max(char board[N][N], char player)
 	return max;
 }
 
+
+/*
+*  Used for computer's logic.
+*
+*/
 int my_min(char board[N][N], char player)
 {
 	int result = check_winner(board, OTHER(player));
@@ -341,6 +443,10 @@ int my_min(char board[N][N], char player)
 	return min;
 }
 
+/*
+*  Used for computer's logic.
+*
+*/
 void minimax(char board[N][N], char player)
 {
 	int i, j, max, max_i, max_j;
@@ -372,228 +478,140 @@ void minimax(char board[N][N], char player)
 			}
 		}
 	}	
-	if(enter_coord_move(board, player, max_i, max_j) == TRUE){
-		printf("Minimax - Error entering move\n"); //Debug
-		exit(0);
-	}
+	
+	enter_coord_move(board, player, max_i, max_j);
+	
+	//if(enter_coord_move(board, player, max_i, max_j) == TRUE){
+		//printf("Minimax - Error entering move\n"); //Debug
+		//exit(0);
+	//}
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//coisas do filipe
-
-
 /*
+* Function that allows the user to choose x or o.
 *
-*
+* @returns char representing the choice
 */
-void random_move(char board[N][N], char ch){
-     int x, y;
+char character_choose(){
+     char ch;
      
-     do{
-         x = rand()%3;
-         y = rand()%3;
-     }while(board[x][y] != EMPTY_CHAR);
+     printf("Do you prefer playing with X or O? \n");
      
-     board[x][y] = ch;
-}
-
+     while(TRUE){
+         scanf("%c", &ch);
+         __fpurge(stdin);
+         ch = toupper(ch);
+         
+         if(ch == PLAYER_X){
+               return PLAYER_X;
+               break;
+         }
+         if(ch == PLAYER_O){
+               return PLAYER_O;
+               break;
+         }
+         if(ch == EXIT) exit(0);
+         
+         printf("Incorrect Choice, Please try again: \n");
+     }
+}     
 /*
+*  Decides randomly who is the first to play
 *
-*
+*  @returns true if the user starts playing, false otherwise
 */
-int block_line_1(char board[N][N], char ch){
-    if(board[0][0] == EMPTY_CHAR){
-       if(((board[0][1] == board[0][2]) && (board[0][1] != EMPTY_CHAR)) ||
-          ((board[1][0] == board[2][0]) && (board[1][0] != EMPTY_CHAR)) ||
-          ((board[1][1] == board[2][2]) && (board[1][1] != EMPTY_CHAR))){
-                        board[0][0] = ch;
-                        return TRUE;
+int who_goes_first(){
+    int n;
+    char ch;
+    
+    printf("Choose between an even and an odd number (e/o): \n");
+    while(TRUE){
+        scanf("%c", &ch);
+        __fpurge(stdin);
+        ch = tolower(ch);
+        if((ch == 'e') || (ch == 'o'))
+            break;
+        if(ch == EXIT) exit(0);
+        printf("Incorrect choice. Try again.\n");
+    }
+    
+    n = rand()%11;
+    if(ch == 'e'){
+          if((n%2) == 0){
+                   printf("You go first!\n");
+                   return TRUE;
+          }
+    }
+    if(ch == 'o'){
+          if((n%2) != 0){
+                   printf("You go first!\n");
+                   return TRUE;
           }
     }
     
-    if(board[0][1] == EMPTY_CHAR){
-       if(((board[0][0] == board[0][2]) && (board[0][0] != EMPTY_CHAR)) ||
-          ((board[1][1] == board[2][1]) && (board[1][1] != EMPTY_CHAR))){
-                        board[0][1] = ch;
-                        return TRUE;
-          }
-    }
-    
-    if(board[0][2] == EMPTY_CHAR){
-       if(((board[0][0] == board[0][1]) && (board[0][0] != EMPTY_CHAR)) ||
-          ((board[1][2] == board[2][2]) && (board[1][2] != EMPTY_CHAR)) ||
-          ((board[1][1] == board[2][0]) && (board[1][1] != EMPTY_CHAR))){
-                        board[0][2] = ch;
-                        return TRUE;
-          }
-    }
-    
-    return FALSE;
-}
-
-
-/*
-*
-*
-*/
-int block_line_2(char board[N][N], char ch){
-    if(board[1][0] == EMPTY_CHAR){
-       if(((board[1][1] == board[1][2]) && (board[1][1] != EMPTY_CHAR)) ||
-          ((board[0][0] == board[2][0]) && (board[0][0] != EMPTY_CHAR))){
-                        board[1][0] = ch;
-                        return TRUE;
-          }
-    }
-    
-    if(board[1][1] == EMPTY_CHAR){
-       if(((board[1][0] == board[1][2]) && (board[1][0] != EMPTY_CHAR)) ||
-          ((board[0][1] == board[2][1]) && (board[0][1] != EMPTY_CHAR)) ||
-          ((board[0][2] == board[2][0]) && (board[0][2] != EMPTY_CHAR))){
-                        board[1][1] = ch;
-                        return TRUE;
-          }
-    }
-    
-    if(board[1][2] == EMPTY_CHAR){
-       if(((board[1][0] == board[1][1]) && (board[1][0] != EMPTY_CHAR)) ||
-          ((board[0][2] == board[2][2]) && (board[0][2] != EMPTY_CHAR))){
-                        board[1][2] = ch;
-                        return TRUE;
-          }
-    }
-    
+    printf("The computer goes first!\n");
     return FALSE;
 }
 
 /*
-*
-*
-*/
-int block_line_3(char board[N][N], char ch){
-    if(board[2][0] == EMPTY_CHAR){
-       if(((board[2][1] == board[2][2]) && (board[2][1] != EMPTY_CHAR)) ||
-          ((board[0][0] == board[1][0]) && (board[0][0] != EMPTY_CHAR)) ||
-          ((board[0][2] == board[1][1]) && (board[0][2] != EMPTY_CHAR))){
-                        board[2][0] = ch;
-                        return TRUE;
-          }
-    }
-    
-    if(board[2][1] == EMPTY_CHAR){
-       if(((board[2][0] == board[2][2]) && (board[2][0] != EMPTY_CHAR)) ||
-          ((board[0][1] == board[1][1]) && (board[0][1] != EMPTY_CHAR))){
-                        board[2][1] = ch;
-                        return TRUE;
-          }
-    }
-    
-    if(board[2][2] == EMPTY_CHAR){
-       if(((board[2][0] == board[2][1]) && (board[2][0] != EMPTY_CHAR)) ||
-          ((board[0][2] == board[1][2]) && (board[0][2] != EMPTY_CHAR)) ||
-          ((board[0][0] == board[1][1]) && (board[0][0] != EMPTY_CHAR))){
-                        board[2][2] = ch;
-                        return TRUE;
-          }
-    }
-    
-    return FALSE;
-}
-
-/*
-*
+*  Counts the number of wins
 *
 */
-int block_player(char board[N][N], char ch){
-    
-    if(block_line_1(board, ch) || block_line_2(board, ch) || block_line_3(board, ch)) return TRUE;
-    return FALSE;
-}
-
-/*
-*
-*
-*/
-int draw_game(char board[N][N]){
-    int i, j;
-    
-    for(i=0; i < N; i++){
-          for(j=0; j < N; j++){
-              if(board[i][j] == EMPTY_CHAR) return FALSE;
-          }
-    }
-    
-    return TRUE;
-} 
- 
-/*
-*
-*
-*/
-int check_winner_2(char board[N][N]){
-    int i;
-    
-    for(i = 0; i < 3; i++){
-          if((board[i][0] == board[i][1]) && (board[i][0] == board[i][2])){
-              return board[i][0];
-          }
-    }
-    
-    for(i = 0; i < 3; i++){
-          if((board[0][i] == board[1][i]) && (board[0][i] == board[2][i])){
-               return board[0][i];
-          }
-    }
-    
-    if((board[0][0] == board[2][2]) && (board[1][1] == board[2][2])){
-         return board[1][1];
-    }
-    if((board[1][1] == board[0][2]) && (board[1][1] == board[2][0])){
-         return board[1][1];
-    }
+void winner_counter(char ch){
      
-    if(draw_game(board)) return 'V';
-    
-    return EMPTY_CHAR;
+     if(ch == PLAYER_X) win_x++;
+     if(ch == PLAYER_O) win_o++;
+     
+     printf("Wins:   X = %d   |   O = %d\n", win_x, win_o);
+
 }
 
 /*
-*
+* Checks if the user wants to keep playing.
 *
 */
-void move_computer(char board[N][N], char ch){
-     if(block_player(board, ch)) return;
-     random_move(board, ch);
-}
 
+int play_again(){
+    char choice;
+    
+    printf("Do you want to play another game? (y/n): \n");
+    
+    while(TRUE){
+        scanf("%c", &choice);
+        __fpurge(stdin);
+        choice = tolower(choice);
+        
+        if(choice == 'y'){
+                  return TRUE;
+                  break;
+        }
+        if(choice == 'n'){
+                  return FALSE;
+                  break;
+        }
+        if(choice == EXIT) exit(0);
+        
+        printf("Incorrect choice. Try Again.\n");
+        
+    }
+    
+    return FALSE;   
+}
+    
 /*
-*
+*  Function with the demonstration (Computer x Computer game) logic
 *
 */
 void computer_computer() {
-     char ch = PLAYER_X; // Somente para teste
-     char final;
+     char ch = PLAYER_X; 
+     int final;
      char board[N][N];
      
      init_board(board);
      do{
-         move_computer(board, ch);
+         comp_move(board, ch);
          print_board(board);
-         final = check_winner_2(board);
+         final = check_winner(board, ch);
          if(ch == PLAYER_X){
              ch = PLAYER_O;
          }
@@ -601,33 +619,45 @@ void computer_computer() {
              ch = PLAYER_X;
          }
          getchar();
-     }while(final == EMPTY_CHAR);
+     }while(final == UNFINISHED);
      
 }
 
+/*
+*  Contains the logic for the player x computer game
+*
+*/
 void user_computer() {
-     char choice;
-     choice = choose_player_character();
+     char choice, computer;
+     char cont;
+     choice = character_choose();
+     if(choice == PLAYER_X) computer = PLAYER_O;
+     if(choice == PLAYER_O) computer = PLAYER_X; 
      char board[N][N];
-     init_board(board);
-     while(TRUE)
-	 {
-		if(choice == PLAYER_X) {
-			if(player_move(board, PLAYER_X) == TRUE)
-				break;
-			if(comp_move(board, PLAYER_O) == TRUE)
-				break;
-		}else{
-			if(comp_move(board, PLAYER_X) == TRUE)
-				break;		
-			if(player_move(board, PLAYER_O) == TRUE)
-				break;
-		}
-	}     
+     while(TRUE){
+             int first = who_goes_first();
+             init_board(board);
+             while(TRUE)
+             {
+                    if(first == TRUE){
+                             if(player_move(board, choice) == TRUE) break;
+                             if (preview_computer_win(board, choice, computer) == TRUE) break;
+                             if(comp_move(board, computer) == TRUE) break;
+                    }
+                    else{
+                         if(comp_move(board, computer) == TRUE) break;
+                         if(player_move(board, choice) == TRUE) break;
+                         if (preview_computer_win(board, choice, computer) == TRUE) break;
+                    }
+             }
+             
+             cont = play_again();
+             if(cont == FALSE) break;
+     }     
 }
  
 /*
-*
+* Initialize the board.
 *
 */
 void init_board(char board[N][N]) {
@@ -640,7 +670,7 @@ void init_board(char board[N][N]) {
 }
 
 /*
-*
+* Prints the board
 *
 */
 void print_board(char board[N][N]) {
